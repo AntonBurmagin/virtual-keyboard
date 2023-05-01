13f0556b95b1e9c05caccb7b65972d7a1337c76b
Anton Burmagin <burmagin.ant.s@gmail.com>
2023-05-01 19:33:50 +0600

    initial

---
+++ app.js
@@ -0,0 +1,6 @@
+// import * as symbols from 'english-symbols';
+import { secsInMin } from './english-symbols';
+console.log(secsInMin);
+
+// for (let symb of symbols)
+//     console.log(symb);
---
+++ english-symbols.js
@@ -0,0 +1,8 @@
+// export const engSymbols = [['`','~'], ['!','1'], ['@','2'], ['#','3'], ['$','4'], ['%','5'], ['^','6']
+//                     ['&','7'], ['*','8'], ['(','9'], [')','0'], ['_','-'], ['+','='], 'Backspace'];
+
+// export function textArea() {
+//     return <textarea></textarea>;
+// }
+export const secsInMin = 60;
+document.body.innerHTML = "<textarea></textarea>";
