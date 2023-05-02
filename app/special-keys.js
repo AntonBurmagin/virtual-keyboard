349ad0c0274f872908ae0ee009ac9ed595768375
Anton Burmagin <burmagin.ant.s@gmail.com>
2023-05-02 06:21:26 +0600

    deploy 4

--- app/special-keys.js
+++
@@ -1,4 +0,0 @@
-export const specialKeyEvent = ['ShiftLeft', 'ShiftRight', 'ControlLeft', 'ControlRight', 'AltLeft', 'AltRight',
-                                'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-
-export const specialKey = ['Shift', 'Shift', 'Ctrl', 'Ctrl', 'Alt', 'Alt', '&uarr;', '&larr;', '&darr;', '&rarr;'];
